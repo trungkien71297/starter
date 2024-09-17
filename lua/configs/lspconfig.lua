f3ec39e670ac1fc97557c63a155611f08b0cff33
trungkien71297 <trungkien71297@gmail.com>
2024-09-17 09:44:18 +0700

    My config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,19 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+  "cssls",
+  "dartls",
+  "pylsp",
+  "fish_lsp",
+  "fish_lsp",
+  "gradle_ls",
+  "groovyls",
+  "jsonls",
+  "kotlin_language_server",
+  "kotlin_lsp",
+  "lua_ls",
+}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
