36da3ce570107a16bcae4fd7990f90b97f774be3
trungkien71297 <trungkien71297@gmail.com>
2024-09-17 09:44:18 +0700

    My config

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,59 @@
+return {
+  "github/copilot.vim",
+  event = "InsertEnter",
+  config = function()
+    -- Disable default tab mapping to avoid conflicts
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_assume_mapped = true
+    vim.g.copilot_tab_fallback = ""
+
+    -- Enable copilot for specific filetypes
+    -- vim.g.copilot_filetypes = {
+    --   ["*"] = false,
+    --   python = true,
+    --   javascript = true,
+    --   typescript = true,
+    --   javascriptreact = true,
+    --   typescriptreact = true,
+    --   lua = true,
+    --   go = true,
+    --   rust = true,
+    --   cpp = true,
+    --   c = true,
+    --   java = true,
+    --   php = true,
+    --   ruby = true,
+    --   vim = true,
+    --   sh = true,
+    --   zsh = true,
+    --   bash = true,
+    --   markdown = true,
+    --   json = true,
+    --   yaml = true,
+    --   toml = true,
+    --   html = true,
+    --   css = true,
+    --   scss = true,
+    --   dart = true,
+    --   kt = true,
+    --   cs = true,
+    -- }
+
+    -- Custom keymaps for copilot
+    local opts = { silent = true, noremap = true }
+
+    -- Accept suggestion
+    vim.keymap.set("i", "<C-l>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
+
+    -- Navigate through suggestions
+    vim.keymap.set("i", "<C-j>", "<Plug>(copilot-next)", opts)
+    vim.keymap.set("i", "<C-k>", "<Plug>(copilot-previous)", opts)
+
+    -- Dismiss suggestion
+    vim.keymap.set("i", "<C-h>", "<Plug>(copilot-dismiss)", opts)
+
+    -- Toggle copilot
+    vim.keymap.set("n", "<leader>cp", ":Copilot panel<CR>", opts)
+    vim.keymap.set("n", "<leader>ct", ":Copilot toggle<CR>", opts)
+  end,
+}
---
+++ lua/plugins/flutter-tool.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-flutter/flutter-tools.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim", -- optional for vim.ui.select
+  },
+  config = true,
+  ft = { "dart" },
+}
